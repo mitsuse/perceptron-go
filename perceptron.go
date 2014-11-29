02922d2cfd726292abf218a076fe41b1e9915cc9
Tomoya Kose <tomoya@mitsuse.jp>
2014-11-29 11:50:02 +0900

    "Perceptron" is a "Learner", which has a method called "Learn".

--- perceptron.go
+++ perceptron.go
@@ -1,1 +1,31 @@
 package perceptron
+
+type Perceptron struct {
+	iteration int
+}
+
+func New(iteration byte) *Perceptron {
+	p := &Perceptron{
+		iteration: int(iteration),
+	}
+
+	return p
+}
+
+func (p *Perceptron) Learn(model Model, iter InstanceIter) error {
+	// TODO: Implement this.
+	return nil
+}
+
+type Learner interface {
+	Learn(model Model, iter InstanceIter) error
+}
+
+type Model interface {
+}
+
+type Instance interface {
+}
+
+type InstanceIter interface {
+}
