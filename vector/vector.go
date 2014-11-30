39ddde79bb28fbcf6ec1d6f1e9d334e779afdf6a
Tomoya Kose <tomoya@mitsuse.jp>
2014-11-30 10:54:58 +0900

    "(Vector).Set" updates a specified element with "index".

--- vector/vector.go
+++ vector/vector.go
@@ -4,6 +4,7 @@ type Vector interface {
 	Size() int
 	Undefined() bool
 	Get(index int) (float64, error)
+	Set(index int, value float64) error
 	Add(vector Vector)
 	Scalar(scalar float64)
 	Dot(vector Vector) (float64, error)
