6281c3714d9c1f48b53728a555f1620798502c8a
Tomoya Kose <tomoya@mitsuse.jp>
2014-12-02 22:43:37 +0900

    Start with simple implementation of trie.

---
+++ trie/simlpe.go
@@ -0,0 +1,20 @@
+package trie
+
+type SimpleTrie struct {
+}
+
+func NewSimpleTrie() *SimpleTrie {
+	t := &SimpleTrie{}
+
+	return t
+}
+
+func (t *SimpleTrie) Get(key []int32) (Trie, bool) {
+	// TODO: Implement this.
+	return nil, false
+}
+
+func (t *SimpleTrie) Update(key []int32) (Trie, bool) {
+	// TODO: Implement this.
+	return nil, false
+}
--- trie/trie.go
+++ trie/trie.go
@@ -1,6 +1,6 @@
 package trie
 
 type Trie interface {
-	Get(key []int32) (Trie, error)
-	Update(key []int32) (Trie, error)
+	Get(key []int32) (Trie, bool)
+	Update(key []int32) (Trie, bool)
 }
